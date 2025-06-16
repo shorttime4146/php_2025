337c6ea5c9855a28ec48229746c8602cf2359495
shorttime4146 <shorttimememoryloss4146@gmail.com>
2025-06-16 00:15:24 -0800

    dashboad er attendence table er insert form

--- MyPhp/php2025/Admin_dashboad/atten_insert_form .php
+++ MyPhp/php2025/Admin_dashboad/atten_insert_form .php
@@ -61,89 +61,107 @@
                   	<!--end::Header-->
                   	<!--begin::Form-->
                   	<form action="emp_insert.php" method="post">
-                    <!--begin::Body-->
-						<div class="card-body">
-							<div class="row mb-3">
-								<label for="emp_name" class="col-sm-2 col-form-label">Employee Name</label>
-								<div class="col-sm-10">
-								<input type="taxt" name="employee_name" class="form-control" id="emp_name" />
+						<!--begin::Body-->
+							<div class="card-body">
+								<div class="row mb-3">
+									<label for="emp_name" class="col-sm-2 col-form-label">Employee Name</label>
+									<div class="col-sm-10">
+									<input type="taxt" name="employee_name" class="form-control" id="emp_name" />
+									</div>
 								</div>
-							</div>
-							<div class="row mb-3">
-								<label for="i_time" class="col-sm-2 col-form-label">In Time</label>
-								<div class="col-sm-10">
-									<input type="time" name="in_time" class="form-control" id="i_time" />
+								<div class="row mb-3">
+									<label for="i_time" class="col-sm-2 col-form-label">In Time</label>
+									<div class="col-sm-10">
+										<input type="time" name="in_time" class="form-control" id="i_time" />
+									</div>
 								</div>
-							</div>
-							<div class="row mb-3">
-								<label for="o_time" class="col-sm-2 col-form-label">Out Time</label>
-								<div class="col-sm-10">
-									<input type="time" name="out_time" class="form-control" id="o_time" />
+								<div class="row mb-3">
+									<label for="o_time" class="col-sm-2 col-form-label">Out Time</label>
+									<div class="col-sm-10">
+										<input type="time" name="out_time" class="form-control" id="o_time" />
+									</div>
 								</div>
-							</div>
-							<div class="row mb-3">
-								<label for="shift_i" class="col-sm-2 col-form-label">Shift In</label>
-								<div class="col-sm-10">
-									<input type="timestamp" name="shift_in" class="form-control" id="shift_i" />
-								</div>
-							</div>	
-							<div class="row mb-3">
-								<label for="shift_o" class="col-sm-2 col-form-label">Shift Out</label>
-								<div class="col-sm-10">
-									<input type="timestamp" name="shift_out" class="form-control" id="shift_o" />
-								</div>
-							</div>						
-							<div class="row mb-3">
-								<label for="over_t" class="col-sm-2 col-form-label">Over Time</label>
-								<div class="col-sm-10">
-									<input type="timestamp" name="over_time" class="form-control" id="over_t" />
-								</div>
-							</div>							
-							
-							<div class="row mb-3">
-								<label for="dayLebel" class="form-label">Day</label>
-								<div class="col-sm-10">
-									<?php
-										$all_weekend_arr = array(1 => "Saturday", 2 => "Sunday", 3 => "Monday", 4 => "Tuesday", 5 => "Wednesday", 6 => "Thursday", 7 => "Friday");
-									?>
-									<select name="day_id" class="form-select" id="dayLebel" >
-										<option value="0">Select Day</option>
+								<div class="row mb-3">
+									<label for="shift_i" class="col-sm-2 col-form-label">Shift In</label>
+									<div class="col-sm-10">
+										<input type="timestamp" name="shift_in" class="form-control" id="shift_i" />
+									</div>
+								</div>	
+								<div class="row mb-3">
+									<label for="shift_o" class="col-sm-2 col-form-label">Shift Out</label>
+									<div class="col-sm-10">
+										<input type="timestamp" name="shift_out" class="form-control" id="shift_o" />
+									</div>
+								</div>						
+								<div class="row mb-3">
+									<label for="over_t" class="col-sm-2 col-form-label">Over Time</label>
+									<div class="col-sm-10">
+										<input type="timestamp" name="over_time" class="form-control" id="over_t" />
+									</div>
+								</div>							
+								
+								<div class="row mb-3">
+									<label for="dayLebel" class="form-label">Day</label>
+									<div class="col-sm-10">
 										<?php
-											foreach($all_weekend_arr as $key => $dayName) {
-											if($key ==  $row['day_id']) {
-												$selected = "selected";
-											} else {
-												$selected = "";
-											}
+											$all_weekend_arr = array(1 => "Saturday", 2 => "Sunday", 3 => "Monday", 4 => "Tuesday", 5 => "Wednesday", 6 => "Thursday", 7 => "Friday");
 										?>
-										<option value="<?php echo $key; ?>"> <?php echo $dayName; ?></option>
+										<select name="day_id" class="form-select" id="dayLebel" >
+											<option value="0">Select Day</option>
+											<?php
+												foreach($all_weekend_arr as $key => $dayName) {
+												if($key ==  $row['day_id']) {
+													$selected = "selected";
+												} else {
+													$selected = "";
+												}
+											?>
+											<option value="<?php echo $key; ?>"> <?php echo $dayName; ?></option>
+											<?php
+												}
+											?>
+										</select>
+									</div>
+								</div>
+								<div class="row mb-3">
+									<label for="monthLebel" class="form-label">Month</label>
+									<div class="col-sm-10">
 										<?php
-											}
+											$all_month_arr = array(1 => "January", 2 => "February", 3 => "March", 4 => "April", 5 => "May", 6 => "June", 
+															7 => "July", 8=>"August", 9 =>"September", 10 =>"October", 11 =>"November", 12 =>"December");
 										?>
-									</select>
+										<select name="month" class="form-control" id="monthLebel">
+											<option value="0">Select Month</option>
+											<?php
+												foreach($all_month_arr as $key => $monthName) {
+												if($key ==  $row['month_id']) {
+													$selected = "selected";
+												} else {
+													$selected = "";
+												}
+											?>
+											<option value="<?php echo $key; ?>"> <?php echo $monthName; ?></option>
+											<?php
+												}
+											?>
+										</select>
+									</div>
 								</div>
+								<div class="row mb-3">
+									<label for="resign_date" class="col-sm-2 col-form-label">Resign Date</label>
+									<div class="col-sm-10">
+										<input type="date" name="resign_date" class="form-control" id="resign_date" />
+									</div>
+								</div>													
 							</div>
-							<div class="row mb-3">
-								<label for="month_lbl" class="col-sm-2 col-form-label">Month</label>
-								<div class="col-sm-10">
-									<input type="month" name="month" class="form-control" id="month_lbl" />
-								</div>
-							</div>
-							<div class="row mb-3">
-								<label for="resign_date" class="col-sm-2 col-form-label">Resign Date</label>
-								<div class="col-sm-10">
-									<input type="date" name="resign_date" class="form-control" id="resign_date" />
-								</div>
-							</div>													
+							<!--end::Body-->
+						<!--begin::Footer-->
+						<div class="card-footer">
+						<button type="submit" class="btn btn-warning">Sign in</button>
+						<button type="submit" class="btn float-end">Cancel</button>
 						</div>
-						<!--end::Body-->
-                    <!--begin::Footer-->
-                    <div class="card-footer">
-                      <button type="submit" class="btn btn-warning">Sign in</button>
-                      <button type="submit" class="btn float-end">Cancel</button>
-                    </div>
-                    <!--end::Footer-->
-                  </form>
+						<!--end::Footer-->
+                  	</form>
                   <!--end::Form-->
                 </div>
                 <!--end::Horizontal Form-->
